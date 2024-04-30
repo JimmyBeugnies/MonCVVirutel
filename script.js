e341cfbd5f91dc353093621e245c064477087a5d
Jimmy Beugnies <beugniesjimmy@gmail.com>
2024-04-30 12:03:50 +0200

    .

---
+++ script.js
@@ -0,0 +1,8 @@
+/* document.addEventListener("DOMContentLoaded", function() {
+  setTimeout(function() {
+    document.querySelector(".loader").classList.add("hidden");
+    document.querySelector("header").classList.remove("hidden");
+    document.querySelector(".Projet").classList.remove("hidden");
+  }, 7000); // 7 secondes
+});
+ */
